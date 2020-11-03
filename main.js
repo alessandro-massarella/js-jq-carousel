868f0eef81114030d8ec45f5af8d05e35b93fe24
alessandro <alessandro.massarella@gmail.com>
2020-11-03 17:07:15 +0100

    0.5
    - scorrimento immagini avanti/indietro + circles

--- main.js
+++ main.js
@@ -10,11 +10,11 @@
 // inserisco tutto il codice per essere sicuro che venga caricato solamente quando il DOM è pronto
 $(document).ready(function(){
 
-// per scorrere il carosello in avanti
+// per scorrere il carosello in AVANTI
   $('.slider-wrapper .next').click(function() {
     var activeImg = $('.slider-wrapper .images img.active');
 
-    // se l'immagine attiva sarà l'ultima, avrà la classe last, a quel punto andrò a dare la classe active alla prima immagine (che ha classe first) in modo tale da avere un carosello circolare 
+    // se l'immagine attiva sarà l'ultima, avrà la classe last, a quel punto andrò a dare la classe active alla prima immagine (che ha classe first) in modo tale da avere un carosello circolare
 
     if (activeImg.hasClass('last')) {
       $('.slider-wrapper .images img.first').addClass('active')
@@ -22,17 +22,43 @@ $(document).ready(function(){
       activeImg.next().addClass('active')
     }
       activeImg.removeClass('active')
+
+      // sezione circle - avanti
+      var activeCircle = $('.slider-wrapper .nav i.active');
+
+          if (activeCircle.hasClass('last')) {
+            $('.slider-wrapper .nav i.first').addClass('active')
+          } else {
+            activeCircle.next().addClass('active')
+          }
+            activeCircle.removeClass('active')
     })
 
-// per scorrere il carosello all'indietro
+
+
+// per scorrere il carosello all'INDIETRO
   $('.slider-wrapper .prev').click(function() {
     var activeImg = $('.slider-wrapper .images img.active');
 
     if (activeImg.hasClass('first')) {
       $('.slider-wrapper .images img.last').addClass('active')
     } else {
-            activeImg.prev().addClass('active')
+      activeImg.prev().addClass('active')
     }
-      activeImg.removeClass('active')
+    activeImg.removeClass('active')
+
+    // sezione circle-INDIETRO
+    var activeCircle = $('.slider-wrapper .nav i.active');
+
+    if (activeCircle.hasClass('first')) {
+      $('.slider-wrapper .nav i.last').addClass('active')
+    } else {
+      activeCircle.prev().addClass('active')
+    }
+    activeCircle.removeClass('active')
+
     })
+
+
+
 })
